759265079c275fba6384cb6b3de28614834142f5
IsmaelArtavia <ismaelartavia@yahoo.es>
2021-02-04 16:45:42 +0000

    actualizacion

---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,16 @@
+import React from "react";
+import proTypes from "prop-types";
+
+export function Jumbotron() {
+	return (
+		<div className="jumbotron">
+			<div className="encima">
+				<h1 className="display-4">Reloj</h1>
+				<p className="lead" />
+
+				<p className="lead" />
+			</div>
+			<div className="text-center" />
+		</div>
+	);
+}
